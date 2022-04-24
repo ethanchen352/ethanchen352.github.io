9b7ee63033c26f6cacdc97fa62c26177e621253d
ethanchen352 <chen5791@umn.edu>
2022-04-24 15:49:56 -0500

    Added files

--- test.js
+++ test.js
@@ -1,4 +1,7 @@
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d');
 
-ctx.fillStyle =
+ctx.beginPath();
+ctx.moveTo(150, 150);
+ctx.lineTo(200, 200);
+ctx.stroke();
